a2be613328a69c2415d1030812babbee0c6665ec
KASTURI SRAVYA <kasturisravya15@gmail.com>
2025-01-28 16:21:55 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,14 @@
+public class Leap {
+        public static void main(String[] args){
+            int year=2020;
+            System.out.println();
+            if((year%400==0)||((year%4==0) &&(year%100!=0))){
+            System.out.println(year+"is a leap year");
+            }
+            else{
+            System.out.println(year+"is not a leap year");
+            }
+            System.out.println();
+       }
+    }
+
